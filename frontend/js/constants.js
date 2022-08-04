a6bb68cb1342a3e253157094ce49bea7a20a0ffe
Michael Wisniewski <info.mbwdesign@gmail.com>
2022-08-03 22:49:30 -0400

    Updated JS Constants

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,22 +1,27 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x645F2e946bE66022b419a61f3FA74086741b4d9B"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!!";
+const welcome_h1 = "MakePaul NFT Collection";
+const welcome_h2 = "A collection of 10,000 NFTs inspired by your favorite celebrity-athlete, Jake Paul!";
+const welcome_p = "Welcome to the MakePaul NFT Collection! A series of 10,000 NFTs inspired by everyone's favorite celebrity-athlete, Jake Paul! Each individual MakePaul is uniquely generated and lives to brawl on the Ethereum blockchain. Unlock your very own unique MakePaul and receive access to various future perks and members-only benefits. New areas and perks will be revealed and can be unlocked through roadmap activation.";
+
+const h1_presale_coming_soon = "Coming Soon!";
+const h1_presale_mint = "Pre-Sale Minting Open!";
+const h1_public_mint = "Public Minting Open!";
+
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
+
 const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
 const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
+
 const button_presale_coming_soon = "Get on the Whitelist";
 const button_presale_mint_whitelisted = "Mint Your Special NFT";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
 const button_public_mint = "Mint Your NFT";
+
 const mint_failed = "Minting failed. 😢 Please try again.";
