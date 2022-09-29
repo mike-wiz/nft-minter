6e2003de9b83873441265564137e103bf3539fde
Michael Wisniewski <info.mbwdesign@gmail.com>
2022-09-28 23:43:54 -0400

    Updated Constants

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -5,25 +5,25 @@ const chain           = 'rinkeby'; // rinkeby, polygon, or ethereum
 const welcome_h1 = "MakePaul NFT Collection";
 const welcome_h2 = "A collection of 10,000 NFTs inspired by your favorite celebrity-athlete, Jake Paul!";
 const welcome_p1 = "Welcome to the MakePaul NFT Collection! A series of 10,000 NFTs inspired by everyone's favorite celebrity-athlete, Jake Paul! Each individual Jake Paul is uniquely generated and lives to brawl on the Ethereum blockchain. Unlock your very own unique Jake Paul to become a member of #Team10k and receive access to future member-only features &amp; benefits.<br><br>Just when you thought one Jake Paul was enough... On October 29th, 2022 all 10,000 unique Jake Paul NFTs will be revealed!";
-const welcome_p2 = ">No fixed fights over here, only fixed prices. Each individual Jake Paul has a set price of <b>0.01171997 ETH</b> to ensure membership costs the same for everyone.";
+const welcome_p2 = "No fixed fights over here, only fixed prices. Each individual Jake Paul has a set price of <b>0.01171997 ETH</b> to ensure membership costs the same for everyone.";
 
-// const h1_presale_coming_soon = "Coming Soon!";
-// const h1_presale_mint = "Pre-Sale Minting Open!";
-// const h1_public_mint = "Public Minting Open!";
-//
-// const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
-// const h2_presale_mint = "Public Minting Countdown";
-//
-// const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
-// const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
-// const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
-// const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-// const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
-//
-// const button_presale_coming_soon = "Get on the Whitelist";
-// const button_presale_mint_whitelisted = "Mint Your Special NFT";
-// const button_presale_mint_not_whitelisted = "Get on the Whitelist";
-// const button_presale_already_minted = "Join The Community";
-// const button_public_mint = "Mint Your NFT";
-//
-// const mint_failed = "Minting failed. 😢 Please try again.";
+const h1_presale_coming_soon = "Coming Soon!";
+const h1_presale_mint = "Pre-Sale Minting Open!";
+const h1_public_mint = "Public Minting Open!";
+
+const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
+const h2_presale_mint = "Public Minting Countdown";
+
+const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
+const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
+const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
+const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
+const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
+
+const button_presale_coming_soon = "Get on the Whitelist";
+const button_presale_mint_whitelisted = "Mint Your Special NFT";
+const button_presale_mint_not_whitelisted = "Get on the Whitelist";
+const button_presale_already_minted = "Join The Community";
+const button_public_mint = "Mint Your NFT";
+
+const mint_failed = "Minting failed. 😢 Please try again.";
